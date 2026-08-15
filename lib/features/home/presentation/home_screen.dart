21cad25b2a7e143f0809eea5e2214cefce59631c
ronishmaharjan <ronish-maharjan@users.noreply.github.com>
2026-08-15 08:36:48 +0545

    fix(export/add-icon): fix icon size

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -277,12 +277,12 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: _exportData,
             icon: const Icon(
               Icons.ios_share,
-              size: 21,
+              size: 18,
             ),),  
         IconButton(
             tooltip: 'Add',
             onPressed: _showAddMenu,
-            icon: const Icon(Icons.add),
+            icon: const Icon(Icons.add,size:18),
           ),
           const SizedBox(width: 4),
         ],
